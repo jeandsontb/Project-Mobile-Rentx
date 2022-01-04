855243a7733563d31b5237e56cff3870477ca9fb
jeandson <jeandsontb@gmail.com>
2022-01-04 18:41:20 -0300

    Implements interface details

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 import { BackButton } from '../../components/BackButton';
 import { ImageSlider } from '../../components/ImageSlider';
 
@@ -7,6 +8,11 @@ import S from './styled';
 const CarDetails = () => {
   return (
     <S.Container>
+      <StatusBar 
+        barStyle="dark-content"        
+        translucent
+        backgroundColor="transparent"
+      />
       <S.BoxHeader>
         <BackButton onPress={() => {}} />
       </S.BoxHeader>
@@ -16,6 +22,26 @@ const CarDetails = () => {
           imagesUrl={['https://cdn.sitewebmotors.com.br/uploads/userGallery/5fcfe53240728.png']} 
         />
       </S.BoxCarImage>
+
+      <S.BoxContent>
+        <S.BoxDetails>
+          <S.BoxDescription>
+            <S.TextBrand>Lamborguini</S.TextBrand>
+            <S.TextName>Huracan</S.TextName>
+          </S.BoxDescription>
+
+          <S.BoxRent>
+            <S.TextPeriod>Ao Dia</S.TextPeriod>
+            <S.TextPrice>R$ 580</S.TextPrice>
+          </S.BoxRent>
+        </S.BoxDetails>
+
+        <S.BoxAbout>
+          Este automóvel desportivo. Surgio o lendário touro lide de vendas e massificado o 
+          melhor carro esportivo da categoria. É um belíssimo carro para quem gosta de 
+          acelerar
+        </S.BoxAbout>
+      </S.BoxContent>
     </S.Container>
   );
 }
--- src/screens/CarDetails/styled.ts
+++ src/screens/CarDetails/styled.ts
@@ -1,4 +1,5 @@
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
 
 export default {
@@ -16,5 +17,49 @@ export default {
   `,
   BoxCarImage: styled.View`
     margin-top: ${getStatusBarHeight() + 32}px;
+  `,
+  BoxContent: styled.ScrollView.attrs({
+    contentContainerStyle: {
+      padding: 24,
+      alignItems: 'center'
+    },
+    showsVerticalScrollIndicator: false
+  })``,
+  BoxDetails: styled.View`
+    width: 100%;
+    flex-direction: row;
+    justify-content: space-between;
+    margin-top: 38px;
+  `,
+  BoxDescription: styled.View``,
+  TextBrand: styled.Text`
+    font-family: ${({theme}) => theme.fonts.secondary_500};
+    color: ${({theme}) => theme.colors.text_detail};
+    font-size: ${RFValue(10)}px;
+    text-transform: uppercase;
+  `,
+  TextName: styled.Text`
+    font-family: ${({theme}) => theme.fonts.secondary_500};
+    color: ${({theme}) => theme.colors.text};
+    font-size: ${RFValue(25)}px;
+  `,
+  BoxRent: styled.View``,
+  TextPeriod: styled.Text`
+    font-family: ${({theme}) => theme.fonts.secondary_500};
+    color: ${({theme}) => theme.colors.text_detail};
+    font-size: ${RFValue(10)}px;
+    text-transform: uppercase;
+  `,
+  TextPrice: styled.Text`
+    font-family: ${({theme}) => theme.fonts.secondary_500};
+    color: ${({theme}) => theme.colors.main};
+    font-size: ${RFValue(25)}px;
+  `,
+  BoxAbout: styled.Text`
+    font-family: ${({theme}) => theme.fonts.primary_400};
+    color: ${({theme}) => theme.colors.text};
+    font-size: ${RFValue(15)}px;
+    text-align: justify;
+    margin-top: 23px;
   `,
 }
