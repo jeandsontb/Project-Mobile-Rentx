82b689e640daa414800884833d7eb12fb3cb5da9
jeandson <jeandsontb@gmail.com>
2022-03-20 11:07:48 -0300

    slider application

--- src/components/ImageSlider/index.tsx
+++ src/components/ImageSlider/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import { FlatList, ViewToken } from 'react-native';
 
 import S from './styled';
 
@@ -6,22 +7,47 @@ interface IImagesProps {
   imagesUrl: string[];
 }
 
+interface ChangePointsImageSlider {
+  viewableItems: ViewToken[];
+  changed: ViewToken[];
+}
+
 const ImageSlider = ({ imagesUrl }: IImagesProps) => {
+  const [ imageIndex, setImageIndex ] = useState(0);
+
+  const changeAppointmentsActive = useRef((info: ChangePointsImageSlider) => {
+    const index = info.viewableItems[0].index!;
+    setImageIndex(index);
+  })
+
   return (
     <S.Container>
       <S.BoxImageIndex>
-        <S.ImageIndex active={true} />
-        <S.ImageIndex active={false} />
-        <S.ImageIndex active={false} />
-        <S.ImageIndex active={false} />
+        {imagesUrl.map((_, index) => (
+          <S.ImageIndex 
+            key={index}
+            active={index === imageIndex} 
+          />
+        ))}
       </S.BoxImageIndex>
 
-      <S.BoxCarImageWrapper>
-        <S.ImageCar 
-          source={{uri: imagesUrl[0]}}
-          resizeMode="contain"
+      
+        <FlatList 
+          data={imagesUrl}
+          keyExtractor={key => key}
+          renderItem={({item}) => 
+          <S.BoxCarImageWrapper>
+            <S.ImageCar 
+              source={{uri: item}}
+              resizeMode="contain"
+            />
+          </S.BoxCarImageWrapper>
+          }
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          onViewableItemsChanged={changeAppointmentsActive.current}
         />
-      </S.BoxCarImageWrapper>
+      
 
     </S.Container>
   );
